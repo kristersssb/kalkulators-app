de2815860b9a308f6351811804944052e3f4cc9d
kristersssb <sbitnevskristers@gmail.com>
2026-01-04 01:12:39 +0200

    Update kalkulators.py

--- kalkulators.py
+++ kalkulators.py
@@ -148,7 +148,7 @@ if st.button("Aprēķināt"):
         temperature_parameters = TEMPERATURE_MODEL_PARAMETERS['sapm']['open_rack_glass_polymer']
 
         panelu_skaits = int(np.ceil((total_annual_consumption_kwh / 950) / 0.41))  # Aptuveni 950 kWh/kW gadā un 410 W panelis
-        system = PVSystem(surface_tilt=30, surface_azimuth=225,
+        system = PVSystem(surface_tilt=30, surface_azimuth=180,
                         module_parameters=module, inverter_parameters=inverter,
                         temperature_model_parameters=temperature_parameters,
                         modules_per_string = panelu_skaits, strings_per_inverter = 1) # x paneļi virknē un y virknes uz invertoru
@@ -371,4 +371,5 @@ if st.session_state["rezultats"] is not None:
         st.pyplot(fig3)
         
 # %%
+
 
